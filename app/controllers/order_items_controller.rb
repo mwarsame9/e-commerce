e71326b59069ef71720379ddc958ff1410a96933
Jin Camou and Mohamed Warsame <jin@>
2017-07-10 11:39:08 -0700

    create cart model and controller

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,51 +1,16 @@
 class OrderItemsController < ApplicationController
-  def index
-    @order_items = OrderItem.all
-  end
-
-  def show
-    @order_item = OrderItem.find(params[:id])
-  end
-
-  def new
-    @order_item = OrderItem.new
-  end
 
   def create
-    @order_item = OrderItem.new(order_item_params)
-    if @order_item.save
-    flash[:notice] = "OrderItem successfully added!"
-      redirect_to  order_items_path
-    else
-      render :new
-    end
+    @order = current_order
+    @item = @order.order_items.new(item_params)
+    @order.save
+    session[:order_id] = @order.id
+    redirect_to products_path
   end
 
-  def edit
-    @order_item = OrderItem.find(params[:id])
-  end
+  private
 
-  def update
-    @order_item= OrderItem.find(params[:id])
-    if @order_item.update(order_item_params)
-      flash[:notice] = "OrderItem successfully updated!"
-      redirect_to order_items_path
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @order_item = OrderItem.find(params[:id])
-    if @order_item.destroy
-      flash[:notice] = "OrderItem successfully removed!"
-      redirect_to order_items_path
-    end
-  end
-
-private
-  def order_item_params
-    # Use strict parameters, replace placeholder info below with your class' actual attributes
-    params.require(:order_item).permit(:attribute1, :attribute2, :attribute3)
+  def item_params
+    params.require(:order_item).permit(:quantity, :product_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root :to => 'products#index'
 
+  resource :cart, only: [:show]
 
-  resources :products do
-      resources :order_items
-  end
+  resources :products
+  resources :order_items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
