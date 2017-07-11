a56f9cf02a92602b25a796fc65bb287904cea562
jin camou and Mohamed Warsame <mohamed@>
2017-07-11 10:32:50 -0700

    style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,17 @@
 Product.destroy_all
+Review.destroy_all
 
 25.times do |index|
-  Product.create!(name: Faker::Commerce.product_name,
-                  price: Faker::Commerce.price)
+  product = Product.create!(name: Faker::Commerce.product_name,
+                            price: Faker::Commerce.price)
+
+    rand(2..5).times do |i|
+      review = product.reviews.create!(author: Faker::TwinPeaks.character,
+                                      content: Faker::Hipster.sentence(3))
+
+    end
+
 end
 
 p "Created #{Product.count} products"
+p "Created #{Review.count} reviews"
